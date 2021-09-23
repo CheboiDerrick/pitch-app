a689758c3b63f06a62e8454dc9cbac86e5e1511e
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-23 16:43:33 +0300

    added templates for login and signout views

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
-
+import os
 
 class Config:
     """
     General configuration parent class
     """
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
     SQLALCHEMY_DATABASE_URI = (
             "postgresql+psycopg2://moringa:Mjamaica101@localhost/pitchesapp"
--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+export SECRET-
+
 python3.8 manage.py server
