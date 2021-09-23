91a49636862ca902dae592beed5d974b05cc89cb
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-23 15:40:55 +0300

    app setup to run server

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from .config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap=Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    # Initializing application
+    app = Flask(__name__)
+
+    # Setting up configuration
+    app.config.from_object(config_options[config_name])
+
+    # Intializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    return app
