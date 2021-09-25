90a58c11840b947b8bf43906b3a6ec871ac1c277
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-25 18:37:22 +0300

    changed production configuration

--- app/config.py
+++ app/config.py
@@ -28,7 +28,12 @@ class ProdConfig(Config):
         Config: The parent configuration class with Generl configuration settings
     """
 
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+        "postgres://", "postgresql://", 1
+    )
+
 
 
 class DevConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY='hiinisecretkey%%%%%%%%'
+export SECRET_KEY='odixgvnpuxvztlqc'
 export MAIL_USERNAME='derrickcheboi@gmail.com'
 export MAIL_PASSWORD='odixgvnpuxvztlqc'
 
